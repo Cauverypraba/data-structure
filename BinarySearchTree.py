97c1695edb0a1694de07569b62f0b755c7ce505e
Cauverypraba <prabasatha221@gmail.com>
2021-05-31 07:50:43 +0530

    Implemented Binary Search Tree traversal's,insertion and deletion operations

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -8,11 +8,32 @@ def search(root, key):
     if root is None or root.val == key:
         return root
     else:
-        if  key > root.val:
+        if key > root.val:
             return search(root.right, key)
     return search(root.left, key)
 
+# Function to do inorder traversal of BST.
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
 
+# Function to do preorder traversal of BST.
+def preorder(root):
+    if root:
+        print(root.val)
+        preorder(root.left)
+        preorder(root.right)
+
+# Function to do postorder traversal of BST.
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val)
+
+# Function to insert a new node in BST.
 def insertElement(root, key):
     if root is None:
         return Node(key)
@@ -21,22 +42,52 @@ def insertElement(root, key):
             return root    
         elif root.val < key:
             root.right = insertElement(root.right, key)
-            # root.right = key
-            # print('...', root.right)
         else:
-            root.left = insertElement(root.left, key)
-            # root.left = key
-            # print('//', root.left)
-    print(root.val)        
+            root.left = insertElement(root.left, key)        
     return root
 
-def inorder(root):
-    if root:
-        print('==',root.val)
-        # print(root.left)
-        inorder(root.left)
-        print('val',root.val)
-        inorder(root.right)
+# This function returns the min key value of a node, 
+# instead of searching an entire tree.
+def minValueNode(node):
+    curr = node
+    while curr.left is not None:
+        curr = node.left
+    return curr    
+
+# Function that removes a node from Binary Search Tree and returns a new root.
+def deleteNode(root, key):
+    if root is None:
+        return root    
+    if key < root.val:
+        root.left = deleteNode(root.left, key)
+    elif key > root.val:
+        root.right = deleteNode(root.right, key)
+    # If key is same as root's key, then this is the node
+    # to be deleted    
+    else:
+        # Node with only one child or no child
+        if root.left == None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right == None:
+            temp = root.left
+            root = None
+            return temp
+
+        # Node with two children:
+        # Get the inorder successor
+        # (smallest in the right subtree)    
+        temp = minValueNode(root.right)
+        
+        # Copy the inorder successor's
+        # content to this node
+        root.val = temp.val  
+        
+        # Delete the inorder successor
+        root.right = deleteNode(root.right, key)   
+    return root            
+        
 
 r = Node(50)
 r = insertElement(r, 30)
@@ -44,7 +95,19 @@ r = insertElement(r, 20)
 r = insertElement(r, 40)
 r = insertElement(r, 70)
 r = insertElement(r, 60)
-r = insertElement(r, 80)            
+r = insertElement(r, 80)
+
+print('Inorder Traversal of BST')
 inorder(r)
-print('r',r.val)
+print('Preorder Traversal of BST')
+preorder(r)
+print('Postorder Traversal of BST')
+postorder(r)
+
+print("\nDelete 20")
+r = deleteNode(r, 20)
+
+print("Inorder traversal of the modified tree")
+inorder(r)
+
 
